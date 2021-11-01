540d421d440d940840e36f10dfc7c090bd3aaacf
mitchel Ketcha <micalvin98@gmail.com>
2021-11-01 06:12:43 +0300

    changes made in the ui

--- src/views/Allfolders.vue
+++ src/views/Allfolders.vue
@@ -1,38 +1,51 @@
 <template>
-  <v-container fluid style="margin: 0px;  width: 50%">
-    <v-row  no-gutters class="mb-6">
-      <v-col tyle="margin: 0px;  padding: 0px;" sm="6" md="5" lg="6">
-        <v-card max-width="344"  
-          outlined
-          tile>
-          <v-treeview
-            v-model="tree"
-            :items="items.data"
-            activatable
-            item-key="name"
-            open-on-click
-          >
-            <template v-slot:prepend="{ item, open }" ,slot="label">
-
-              <v-icon v-if="!item.file == true" >
-                {{ open ? "mdi-folder-open" : "mdi-folder" }}
-
-              </v-icon>
-
-              <v-icon v-else>
-                {{ files[documents.path] }}
-              </v-icon>
-            </template>
-          </v-treeview>
-        </v-card>
-      </v-col>
-      <v-col  sm="6" md="5" offset-md="2" lg="6" offset-lg="0">
-        <v-card   class="pa-2">
-          {{documents.path}}
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <v-content>
+      <v-container>
+        <v-row no-gutters>
+          <v-col md="3">
+            <v-card>
+              <v-card-title>
+                Folder List
+              </v-card-title>
+              <v-card-text>
+                <v-treeview
+                  v-model="tree"
+                  :items="items.data"
+                  activatable
+                  item-key="name"
+                  open-on-click
+               
+                >
+                  <template slot="prepend" slot-scope="{ item, open }">
+                    <v-icon v-if="item">
+                      {{ open ? "mdi-folder-open" : "mdi-folder" }}
+                    </v-icon>
+                  </template>
+                </v-treeview>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col md="9">
+            <v-card>
+              <v-card-title>
+                Document List
+              </v-card-title>
+              <v-card-text>
+                <v-treeview :items="documents">
+                  <template slot="prepend" slot-scope="{ item }">
+                    <v-icon v-if="item">
+                      {{ files[item.type] }}
+                    </v-icon>
+                  </template>
+                </v-treeview>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 <script>
 import axios from "axios";
@@ -46,39 +59,53 @@ export default {
         md: "mdi-language-markdown",
         pdf: "mdi-file-pdf",
         png: "mdi-file-image",
-        doc: "mdi-file-document-outline",
+        txt: "mdi-file-document-outline",
         xls: "mdi-file-excel",
       },
       tree: [],
       items: [],
-      documents:[],
+      documents: [
+        {
+          title: "js",
+
+          path: "/okm:root/Evaluations/BOARD EVALUATION(SELF)- Total Board.doc",
+
+          type: "js",
+        },
+        {
+          title: "doc2",
+
+          path: "/okm:root/Evaluations/BOARD EVALUATION(SELF)- Total Board.doc",
+
+          type: "png",
+        },
+        {
+          title: "json",
+
+          path: "/okm:root/Evaluations/BOARD EVALUATION(SELF)- Total Board.doc",
+
+          type: "html",
+        },
+        {
+          title: "md",
+
+          path: "/okm:root/Evaluations/BOARD EVALUATION(SELF)- Total Board.doc",
+
+          type: "js",
+        },
+      ],
     };
   },
-
   mounted() {
-    this.loadTree();
+    this.loadtree();
   },
+
   methods: {
-    async loadTree() {
-      axios
-
-        .get("http://127.0.0.1:8000/api/get-folders/")
-
-        .then((response) => (this.items = response.data))
-
-        .catch((error) => {
-          console.log(error);
-
-          this.errored = true;
-        });
-    },
-    listDocuments: function(item) {
-      axios
-      .get("27.0.0.1:8000/api/get-document-children/" + item.id )
-      .then((response)=>(this.documents = response.data))
-      .catch((error) => {
-        console.log(error);
-        this.erroed = true;
+    async loadtree() {
+      axios.get(`http://127.0.0.1:8000/api/get-folders`).then((response) => {
+        if (response.data.success) {
+          this.items = response.data;
+        }
       });
     },
   },
